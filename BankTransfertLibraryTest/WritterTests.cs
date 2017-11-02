01f3312bc3add6a7eef3d884d1ee183524cb883c
Charles CARON <2012370@campusid.eu>
2017-11-02 10:24:07 +0100

    Writter tests

--- BankTransfertLibraryTest/WritterTests.cs
+++ BankTransfertLibraryTest/WritterTests.cs
@@ -2,6 +2,7 @@ using BankTranfertLibrary;
 using BankTranfertLibrary.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
 
 namespace BankTransfertLibraryTest
 {
@@ -13,6 +14,64 @@ namespace BankTransfertLibraryTest
         {
             var bankTransfert = new BankTransfert();
             bankTransfert.Transfert(1, 12.2m, "4514561", "8546129856");
+        }
+
+        [TestMethod]
+        public void FileExists()
+        {
+            //Arrange
+            IWritter writter = new MyWritter();
+            var title = $"transaction_{DateTime.Now.ToString("dd_MM_yy")}.csv";
+
+            //Act
+            writter.WriteToCsv(1, 10, "From Bank IBAN", "To Bank IBAN");
+
+            //Assert
+            Assert.IsTrue(File.Exists(title));
+        }
+
+        [TestMethod]
+        public void HeaderCheck()
+        {
+            //Arrange
+            IWritter writter = new MyWritter();
+            var title = $"transaction_{DateTime.Now.ToString("dd_MM_yy")}.csv";
+            string firstLine;
+
+            //Act
+            writter.WriteToCsv(1, 10, "From Bank IBAN", "To Bank IBAN");
+
+            using (StreamReader sr = new StreamReader(title))
+            {
+                firstLine = sr.ReadLine();
+                sr.Close();
+            }
+
+            //Assert
+            Assert.AreEqual("Transaction;Amount;From;To", firstLine);
+        }
+
+        [TestMethod]
+        public void WriteInFile()
+        {
+            //Arrange
+            IWritter writter = new MyWritter();
+            var title = $"transaction_{DateTime.Now.ToString("dd_MM_yy")}.csv";
+            string lastLine = "";
+
+            //Act
+            writter.WriteToCsv(2, 20, "From Bank IBAN", "To Bank IBAN");
+            using (StreamReader sr = new StreamReader(title))
+            {
+                while (sr.EndOfStream == false)
+                {
+                    lastLine = sr.ReadLine();
+                }
+                sr.Close();
+            }
+
+            //Assert
+            Assert.AreEqual("2;20;From Bank IBAN;To Bank IBAN", lastLine);
         }
     }
 }
