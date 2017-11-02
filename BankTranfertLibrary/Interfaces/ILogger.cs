acddcaf6c5681edd7b0deb3e8a2581dd82988c37
Charles CARON <2012370@campusid.eu>
2017-11-02 09:19:27 +0100

    Add solution

---
+++ BankTranfertLibrary/Interfaces/ILogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankTranfertLibrary.Interfaces
+{
+    public interface ILogger
+    {
+        void Log(Severity severity, string message);
+    }
+}
